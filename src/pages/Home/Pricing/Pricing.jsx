bcb0882e748cbc911c939ac7e8ea1ada854aede2
unknown <ce.rumiakter@gmail.com>
2025-11-29 18:36:53 +0600

    add footer

--- src/pages/Home/Pricing/Pricing.jsx
+++ src/pages/Home/Pricing/Pricing.jsx
@@ -2,7 +2,7 @@
 
 const Pricing = () => {
     return (
-        <div className="bg-[#F3F3FF]">
+        <div className="bg-[#F3F3FF] mb-12">
             <div className="container mx-auto py-20">
                 <h1 className="text-3xl font-bold text-center">Choose Awesome Plan for your Needs</h1>
             </div>
--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -2,8 +2,44 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is Footer</h1>
+        <div className='bg-[#100B94]'>
+            <footer className="footer sm:footer-horizontal container mx-auto text-neutral-content p-10">
+                <nav>
+                    <h6 className="footer-title">Contact</h6>
+                    <a className="link link-hover">Dhaka Office: Dhaka,Bangladesh</a>
+                    <a className="link link-hover">New York Office: New York, NY 10036 </a>
+                    <a className="link link-hover">Las Vegas: Las Vegas, NV 89109</a>
+                    <a className="link link-hover">Phone: 013XXXXXXXX</a>
+                    <a className="link link-hover">Email: info@gmail.com</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Useful Links</h6>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Privacy Policy</a>
+                    <a className="link link-hover">Returris</a>
+                    <a className="link link-hover">Terms & Conditions</a>
+                    <a className="link link-hover">Latest News</a>
+                    <a className="link link-hover">Our Sitemap</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Our Store</h6>
+                    <a className="link link-hover">New York</a>
+                    <a className="link link-hover">London SF</a>
+                    <a className="link link-hover">Cockfosters BP</a>
+                    <a className="link link-hover">Los Angeles</a>
+                    <a className="link link-hover">Chicago</a>
+                    <a className="link link-hover">Las Vegas</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Follow Us</h6>
+                    <a className="link link-hover">Facebook</a>
+                    <a className="link link-hover">Instagram</a>
+                    <a className="link link-hover">Youtube</a>
+                    <a className="link link-hover">Linkedin</a>
+                    <a className="link link-hover">Google Plus</a>
+                    <a className="link link-hover">Medium</a>
+                </nav>
+            </footer>
         </div>
     );
 };
