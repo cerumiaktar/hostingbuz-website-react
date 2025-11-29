6d090572b3a0e50bd759a003f823719d984199dd
unknown <ce.rumiakter@gmail.com>
2025-11-29 18:20:59 +0600

    add pricing section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
 import DomainSection from '../DomainSection/DomainSection';
+import Pricing from '../Pricing/Pricing';
 
 const Home = () => {
     return (
         <div>
             <Hero></Hero>
             <DomainSection></DomainSection>
+            <Pricing></Pricing>
         </div>
     );
 };
---
+++ src/pages/Home/Pricing/Pricing.jsx
@@ -0,0 +1,13 @@
+
+
+const Pricing = () => {
+    return (
+        <div className="bg-[#F3F3FF]">
+            <div className="container mx-auto py-20">
+                <h1 className="text-3xl font-bold text-center">Choose Awesome Plan for your Needs</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Pricing;
