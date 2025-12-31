7c00e7233e97a383fbb2a7d0bc29e4b4b4541536
unknown <ce.rumiakter@gmail.com>
2025-12-31 16:07:12 +0600

    update datacenter

--- src/pages/Home/DataCenter/DataCenter.jsx
+++ src/pages/Home/DataCenter/DataCenter.jsx
@@ -4,23 +4,24 @@ const DataCenter = () => {
     return (
         <div className="container mx-auto mb-12">
             <div className="flex flex-col md:flex-row">
-                <div className="md:w-6/12">
+                <div className="md:w-6/12 ">
                     <img src="https://i.ibb.co.com/S4z4BX6H/image-2.png" alt="" />
                 </div>
-                <div className="md:w-6/12">
-                    <div>
+                <div className="md:w-6/12 ">
+                    <div className="mb-6 space-y-2">
                         <h4 className="font-bold text-[#F3345B]">Global Datacenters</h4>
                         <h1 className="text-2xl font-bold">Powerful Severs, High Speed Datacenter Connectivity</h1>
                         <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incid idunt ut labore.</p>
                     </div>
-                    <div>
-                        <div>
-                            <h1>Global Datacenters</h1>
-                            <p>Lorem ipsum dolor sit amet consecteturadipis labore dolore magna aliqua</p>
+                    <div className="flex gap-3">
+                        <div className="bg-[#F3F3FF] p-4 rounded-lg">
+                            <BsDatabaseFillLock className="text-2xl text-[#100B94]" />
                         </div>
                         <div>
-                            <BsDatabaseFillLock />
+                            <h1 className="text-xl font-bold ">Global Datacenters</h1>
+                            <p className="text-[#333333]">Lorem ipsum dolor sit amet consecteturadipis labore dolore magna aliqua</p>
                         </div>
+
                     </div>
                 </div>
             </div>
