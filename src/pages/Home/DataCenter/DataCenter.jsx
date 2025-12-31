15afa174f5a2e685a172df1f1298e0ab11425b3a
unknown <ce.rumiakter@gmail.com>
2025-12-31 16:39:09 +0600

    update

--- src/pages/Home/DataCenter/DataCenter.jsx
+++ src/pages/Home/DataCenter/DataCenter.jsx
@@ -1,10 +1,11 @@
 import { BsDatabaseFillLock } from "react-icons/bs";
 import { FaFileCode } from "react-icons/fa";
+import { MdCall } from "react-icons/md";
 
 const DataCenter = () => {
     return (
         <div className="container mx-auto mb-12">
-            <div className="flex flex-col md:flex-row">
+            <div className="flex flex-col md:flex-row items-center">
                 <div className="md:w-6/12 ">
                     <img src="https://i.ibb.co.com/S4z4BX6H/image-2.png" alt="" />
                 </div>
@@ -24,7 +25,7 @@ const DataCenter = () => {
                         </div>
 
                     </div>
-                    <div className="flex gap-3">
+                    <div className="flex gap-3 mb-6">
                         <div className="bg-[#F3F3FF] p-4 rounded-lg">
                             <FaFileCode className="text-2xl text-[#100B94]" />
                         </div>
@@ -32,7 +33,18 @@ const DataCenter = () => {
                             <h1 className="text-xl font-bold ">Facility Security</h1>
                             <p className="text-[#333333]">Lorem ipsum dolor sit amet consecteturadipis labore dolore magna aliqua</p>
                         </div>
-
+                    </div>
+                    <div className="flex items-center">
+                        <button className="py-3 px-4 rounded-md mx-4 bg-[#100B94] text-white text-xl font-medium">Contact Us</button>
+                        <div className="flex gap-2">
+                            <div className="bg-[#100B94] p-4 rounded-lg">
+                                <MdCall className="text-2xl text-white" />
+                            </div>
+                            <div>
+                                <h2 className="text-lg font-bold">Any Query</h2>
+                                <p className="font-medium">++8801844-910755</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
