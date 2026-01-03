0051bec15a0edf4a582c8a465be1e89b1a100ee6
unknown <ce.rumiakter@gmail.com>
2026-01-03 10:21:51 +0600

    update

--- src/pages/Home/Support/Support.jsx
+++ src/pages/Home/Support/Support.jsx
@@ -4,25 +4,27 @@ const Support = () => {
     return (
         <div className="bg-[#F3F3FF] py-20">
             <div className="container mx-auto">
-                <div className="card w-96 bg-base-100 card-md shadow-sm">
-                    <div className="card-body">
-                        <GrCluster />
-                        <h2 className="card-title">IP Transit / Connectivity</h2>
-                        <p>Lorem ipsum dolor amet consecteturincididunt ut labore.</p>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                    <div className="card  bg-base-100 card-md shadow-sm">
+                        <div className="card-body">
+                            <GrCluster />
+                            <h2 className="card-title">IP Transit / Connectivity</h2>
+                            <p>Lorem ipsum dolor amet consecteturincididunt ut labore.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="card w-96 bg-base-100 card-md shadow-sm">
-                    <div className="card-body">
-                        <GrCluster />
-                        <h2 className="card-title">IP Transit / Connectivity</h2>
-                        <p>Lorem ipsum dolor amet consecteturincididunt ut labore.</p>
+                    <div className="card bg-base-100 card-md shadow-sm">
+                        <div className="card-body">
+                            <GrCluster className="text-4xl" />
+                            <h2 className="card-title">Monitoring</h2>
+                            <p>Risus commodo viverra maecena editaccumsan lacus vel facilisis.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="card w-96 bg-base-100 card-md shadow-sm">
-                    <div className="card-body">
-                        <GrCluster />
-                        <h2 className="card-title">IP Transit / Connectivity</h2>
-                        <p>Lorem ipsum dolor amet consecteturincididunt ut labore.</p>
+                    <div className="card bg-base-100 card-md shadow-sm">
+                        <div className="card-body">
+                            <GrCluster />
+                            <h2 className="card-title">24x7 Support</h2>
+                            <p>Duis aute irure dolor in reprehenderitdolore nulla pariatur.</p>
+                        </div>
                     </div>
                 </div>
             </div>
