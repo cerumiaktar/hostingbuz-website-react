087017e47bc71cd52a7cccd950e00162af051dab
unknown <ce.rumiakter@gmail.com>
2026-01-04 11:44:42 +0600

    add blog section

--- src/pages/Home/Blogs/Blogs.jsx
+++ src/pages/Home/Blogs/Blogs.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Blog from './Blog';
 
 const Blogs = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    const [length, setLength] = useState([4]);
+
+    useEffect(() =>{
+        fetch('blogs.json')
+        .then(res=>res.json())
+        .then(data=>setBlogs(data))
+    } ,[])
     return (
         <div className='container mx-auto mb-12'>
-            <div className='flex items-center justify-between'>
+            <div className='flex items-center justify-between mb-6'>
                 <h1 className='text-3xl font-bold'>Recent Blogs</h1>
                 <p>See all</p>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-4'>
+                {
+                    blogs.slice(0, length).map((blog) =><Blog blog={blog}></Blog>)
+                }
             </div>
         </div>
     );
