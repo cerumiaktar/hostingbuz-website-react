6a8959b9ddb754c4f0f67abeb38c5d0583873e01
unknown <ce.rumiakter@gmail.com>
2026-01-04 11:55:35 +0600

    update

--- src/pages/Home/Blogs/Blog.jsx
+++ src/pages/Home/Blogs/Blog.jsx
@@ -12,7 +12,7 @@ const Blog = ({ blog }) => {
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">{title}</h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <p>{short_description}</p>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Buy Now</button>
                     </div>
--- src/pages/Home/Blogs/Blogs.jsx
+++ src/pages/Home/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Blog from './Blog';
+import { Link } from 'react-router-dom';
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
@@ -15,7 +16,7 @@ const Blogs = () => {
         <div className='container mx-auto mb-12'>
             <div className='flex items-center justify-between mb-6'>
                 <h1 className='text-3xl font-bold'>Recent Blogs</h1>
-                <p>See all</p>
+                <Link to='/blogs'><p>See all</p></Link>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-4 gap-4'>
                 {
