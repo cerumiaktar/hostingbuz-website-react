3c44fd038293b946274ef8f61c64c8de67f9633a
unknown <ce.rumiakter@gmail.com>
2026-01-07 17:27:22 +0600

    update review section

--- src/pages/Home/Reviews/Review.jsx
+++ src/pages/Home/Reviews/Review.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const Review = ({review}) => {
-    const {img,title,designation,rating,} = review;
+const Review = ({ review }) => {
+    const { img, title, designation, rating, } = review;
     return (
         <div>
-            
+            <div className="card bg-base-100 card-md shadow-sm">
+                <div className="card-body">
+                    <h2 className="card-title">{title}</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="justify-end card-actions">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Home/Reviews/Reviews.jsx
+++ src/pages/Home/Reviews/Reviews.jsx
@@ -10,9 +10,9 @@ const Reviews = () => {
         .then(data=>setReviews(data))
     } ,[])
     return (
-        <div>
+        <div className='container mx-auto mb-12'>
             <h1 className='text-3xl font-bold text-center mb-6'>Our Happy Customers</h1>
-            <div>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-6'>
                 {
                     reviews.map((review) =><Review review={review}></Review>)
                 }
