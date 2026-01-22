c1adf82bb5c4c9c354720fe99de2ad12acb7c266
unknown <ce.rumiakter@gmail.com>
2026-01-22 10:31:48 +0600

    update

--- src/pages/Home/Reviews/Review.jsx
+++ src/pages/Home/Reviews/Review.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 
 const Review = ({ review }) => {
-    const { img, title, designation, rating,description } = review;
+    const { img, title, designation, rating, description } = review;
     return (
         <div>
             <div className="card bg-base-100 card-md shadow-sm">
                 <div className="card-body">
-                    <div className='flex items-center gap-3'>
-                        <img className='w-12 h-12' src={img} alt="" />
+                    <div className='flex justify-between'>
+                        <div className='flex items-center gap-3'>
+                            <img className='w-12 h-12' src={img} alt="" />
+                            <div>
+                                <h2 className="card-title">{title}</h2>
+                                <p>{designation}</p>
+                            </div>
+                        </div>
                         <div>
-                            <h2 className="card-title">{title}</h2>
-                            <p>{designation}</p>
+                            <span className='text-orange-500 text-lg'>{rating}</span>
                         </div>
                     </div>
                     <div>
